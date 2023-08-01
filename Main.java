4e0f117445d4aa9e419bff070a52a96e3078bb5b
exsaikhtar <141111251+exsaikhtar@users.noreply.github.com>
2023-08-01 12:38:17 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,57 +2,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.mycompany.exaikhtar1;
-//EXA IKHTAR ALDIANSYAH
-//227064516032
-class Book {
-    private String title;
-    private String author;
-    private String price;
-    private String isbn;
-    private String year;
-
-    // Getter
-    public String getTitle() {
-        return title;}
-    public String getAuthor() {
-        return author;}
-    public String getPrice() {
-        return price; }
-    public String getIsbn() {
-        return isbn;}
-    public String getYear() {
-        return year;}
-    // Setter
-    public void setTitle(String title) {
-        this.title = title;}
-
-    public void setAuthor(String author) {
-        this.author = author;}
-    public void setPrice(String price) {
-        this.price = price;}
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;}
-    public void setYear(String year) {
-        this.year = year;
-    }
-}
+package com.mycompany.exa2;
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book();
-        book1.setTitle("les matematika");
-        book1.setAuthor("Exa");
-        book1.setPrice("Rp.250.000");
-        book1.setIsbn("923-227064516032");
-        book1.setYear("2020");
+        // Pegawai
+        Pegawai p1 = new Pegawai("John", "Laki-laki", "101", true);
+        Pegawai p2 = new Pegawai("Jane", "Perempuan", "102", false);
 
-        System.out.println("Book Title: " + book1.getTitle());
-        System.out.println("Author: " + book1.getAuthor());
-        System.out.println("Price: " + book1.getPrice());
-        System.out.println("ISBN: " + book1.getIsbn());
-        System.out.println("Year: " + book1.getYear());
+        System.out.println("Pegawai 1:");
+        p1.toShow();
+        System.out.println("Tunjangan: " + p1.getTunjangan());
+        System.out.println();
 
-        book1.setPrice("Rp.300.000");
-        System.out.println("Updated Price: " + book1.getPrice());
+        System.out.println("Pegawai 2:");
+        p2.toShow();
+        System.out.println("Tunjangan: " + p2.getTunjangan());
+        System.out.println();
+
+        // Programmer
+        Programmer pr1 = new Programmer("Mike", "Laki-laki", "201", false, 5, 5000000);
+        Programmer pr2 = new Programmer("Anna", "Perempuan", "202", true, 2, 4500000);
+        Programmer pr3 = new Programmer("Tom", "Laki-laki", "203", true, 12, 8000000);
+
+        System.out.println("Programmer 1:");
+        pr1.toShow();
+        System.out.println("Tunjangan: " + pr1.getTunjangan());
+        System.out.println("Bonus: " + pr1.getBonus());
+        System.out.println();
+
+        System.out.println("Programmer 2:");
+        pr2.toShow();
+        System.out.println("Tunjangan: " + pr2.getTunjangan());
+        System.out.println("Bonus: " + pr2.getBonus());
+        System.out.println();
+
+        System.out.println("Programmer 3:");
+        pr3.toShow();
+        System.out.println("Tunjangan: " + pr3.getTunjangan());
+        System.out.println("Bonus: " + pr3.getBonus());
+    }
 }
-}
+                                                        
